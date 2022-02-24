83d1aaedfa4d0c25e081fa96688f40aa35852b52
pablo <pgnieto@paradigmadigital.com>
2022-02-24 14:28:42 +0100

    Loading states with racing data third (last part)

--- applications/loading-states/script.js
+++ applications/loading-states/script.js
@@ -11,20 +11,29 @@ import {
   fetchData,
 } from './utilities';
 
-//Second scenario: There's a delay
-
-const showLoading$ = of(true).pipe(
-  delay(+showLoadingForAtLeastField.value),
-  tap(() => showLoading(true))
-)
-
-const hideLoading$ = of(true).pipe(
-  delay(+showLoadingAfterField.value),
-  tap(() => showLoading(false))
-)
+//Third scenario: Racing Data between whether or not show the loading and whether the data came back
 
 const loading$ = fromEvent(form, 'submit').pipe(
-  exhaustMap(() => concat(showLoading$, fetchData(), hideLoading$)),
+  exhaustMap(() => {
+    const data$ = fetchData().pipe(shareReplay(1));
+
+    const showLoading$ = of(true).pipe(
+      delay(+showLoadingAfterField.value),
+      tap((value) => showLoading(value))
+    )
+    
+    const timeToHideTheLoading$ = timer(+showLoadingForAtLeastField.value).pipe(
+      first()
+    );
+
+    const shouldShowLoading$ = concat(//Concat because we through a event into 
+      showLoading$,//the future of showing the loading, soon as we show the loading 
+      timeToHideTheLoading$,//then we set a timer for the bare minimum to take it down
+      data$.pipe(tap(() => showLoading(false)))//then if we don't have the data, keep waiting for it and take down the loading
+    );
+
+    return race(data$, shouldShowLoading$)//But if the data comes back before any of this, it wins(race)
+  }),
 );
 
 loading$.subscribe();
