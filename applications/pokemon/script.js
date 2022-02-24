3cfd12e821ddb03390169eddd8459059768c63d5
pablo <pgnieto@paradigmadigital.com>
2022-02-24 11:56:30 +0100

    Second part Async UI

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -36,14 +36,16 @@ import {
 const endpoint = 'http://localhost:3333/api/pokemon/search/';
 
 const search$ = fromEvent(search, 'input').pipe(
-  map((event) => event.target.value),  
-  mergeMap((searchTerm) => {
-    return fromFetch(endpoint + searchTerm).pipe(
+  debounceTime(300),//CAVEAT this makes the fetch only after 300ms so we wouldn't need the "switchMap((searchTerm)..." below, just a "mergeMap((searchTerm)...."
+  map((event) => event.target.value),
+  switchMap((searchTerm) => {//As now we have some delay we don't care about the previous request "p".."pi"..."pik" So we use switchMap
+    //switchMap not only don't care about that petitions it cancels them either.
+    return fromFetch(endpoint + searchTerm + '?delay=1000&chaos=true').pipe(//'?delay=1000&chaos=true'=> coerced to see behaviour 
       mergeMap((response) => response.json())
     );
   }),
   tap(clearResults),
-  pluck('pokemon'),//"pokemon" is the key of the array in the json response
+  pluck('pokemon'),//"pokemon" is the key of the array in the json response(we want just the array not all the json object of each search)
   tap(addResults),
 );
 
