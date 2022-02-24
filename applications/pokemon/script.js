89a043cf0ceabf35ad53fc13086fdad5dc76a065
pablo <pgnieto@paradigmadigital.com>
2022-02-24 11:14:56 +0100

    first part Async UI

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -14,6 +14,7 @@ import {
   concat,
   take,
   EMPTY,
+  pluck
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -27,4 +28,24 @@ import {
   form,
 } from '../pokemon/utilities';
 
-const endpoint = 'http://localhost:3333/api/pokemon?delay=100';
+/* ACHTUNG!!! */
+//If while working with this file, 
+//this error appears in the console:"Cannot read property 'prototype' of undefined"
+//checked the top line of this file, if it's => <import  from 'express'>, just remove it.
+
+const endpoint = 'http://localhost:3333/api/pokemon/search/';
+
+const search$ = fromEvent(search, 'input').pipe(
+  map((event) => event.target.value),  
+  mergeMap((searchTerm) => {
+    return fromFetch(endpoint + searchTerm).pipe(
+      mergeMap((response) => response.json())
+    );
+  }),
+  tap(clearResults),
+  pluck('pokemon'),//"pokemon" is the key of the array in the json response
+  tap(addResults),
+);
+
+search$.subscribe();
+
